aba903ed7b294d0a3e1a02a2b247354dfcd1e735
Vladimir Tikhonov <vladimir@tikhonov.by>
2017-10-29 17:45:46 +0300

    Added three.js example code

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -1,3 +1,26 @@
-const itWorks = document.createElement('div');
-itWorks.innerHTML = 'It works!';
-document.getElementById('main')!.appendChild(itWorks);
+import * as three from 'three';
+
+const scene = new three.Scene();
+const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new three.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new three.BoxGeometry(1, 1, 1);
+const material = new three.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new three.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const animate = () => {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+
+    renderer.render(scene, camera);
+};
+
+animate();
